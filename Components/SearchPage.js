405110add1fe4c6f608709e189fae4f3aec0e5c1
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-03-31 01:22:46 +0530

    Not Working and occuring Error

---
+++ Components/SearchPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopTutor from "./HomePage/TopTutors";
+
+const SearchPage = () => {
+    return (
+        <div className="full-page-snap-main">
+            <TopTutor />
+        </div>
+    )
+}
+
+export default SearchPage;
